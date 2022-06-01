0adde6eda2b59d6a7555b113685bd47238bbf891
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-06-01 20:16:47 +0100

    Logic for first 2 questions complete and working
    - Installed Nodemon package as a developer dependancy
    
    - Started working on index.js
    
    - Imported inquirer into index.js
    
    - Created a questions array, created the first 2 questions for testing purposes
    
    - Created an init function to start asking the user once the application is run
    
    - confirmed working - prints questions to terminal, and returns an an object of the questions and the users answers in a pair

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 console.log("hello from node");
+
+// 1. import inquirer
+const inquirer = require("inquirer");
+
+// 2. declare your questions array
+const questions = [
+	{
+		type: "input",
+		name: "projectTitle",
+		message: "Please enter is your project title:",
+	},
+	{
+		type: "input",
+		name: "description",
+		message: "Please enter a description of your project:",
+	},
+];
+
+// 3. declare your init function to ask questions
+const init = async () => {
+	// get answers for first set of questions
+	const answers = await inquirer.prompt(questions);
+
+	// display answers
+	console.log(answers);
+};
+
+// 4. start your application
+init();
